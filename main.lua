44c4e63b448efd2ee3de605d1777ea87714a276d
HansRCode <12411150+HansRCode@users.noreply.github.com>
2023-11-22 11:10:26 +0100

    Add images

--- main.lua
+++ main.lua
@@ -6,3 +6,36 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 280
 
+local background = love.graphics.newImage('background.png')
+local ground = love.graphics.newImage('ground.png')
+
+function love.load()
+
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    love.window.setTitle('Fifty Bird')
+
+    push:setScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true, 
+        fullscreen = false,
+        resizeble = true
+    })
+
+end
+
+function love.resize(w,h)
+    push:resize(w,h)
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+function love.draw()
+    push:start()
+    love.graphics.draw(background, 0,0)
+    love.graphics.draw(ground, VIRTUAL_HEIGHT-16)
+    push:finish()
+end
