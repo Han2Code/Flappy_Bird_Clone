387600f78d6e718dec0195588f03280b353737d5
HansRCode <12411150+HansRCode@users.noreply.github.com>
2023-11-24 13:45:35 +0100

    Add background and ground

--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@ WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
 VIRTUAL_WIDTH = 512
-VIRTUAL_HEIGHT = 280
+VIRTUAL_HEIGHT = 288
 
 local background = love.graphics.newImage('background.png')
 local ground = love.graphics.newImage('ground.png')
@@ -15,7 +15,7 @@ function love.load()
 
     love.window.setTitle('Fifty Bird')
 
-    push:setScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true, 
         fullscreen = false,
         resizeble = true
@@ -36,6 +36,6 @@ end
 function love.draw()
     push:start()
     love.graphics.draw(background, 0,0)
-    love.graphics.draw(ground, VIRTUAL_HEIGHT-16)
+    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
     push:finish()
 end
