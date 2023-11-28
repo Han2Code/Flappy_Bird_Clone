96976c086d11f184c8ea99976f68c4f793bc17dc
HansRCode <12411150+HansRCode@users.noreply.github.com>
2023-11-28 11:39:07 +0100

    Bird gravity

--- main.lua
+++ main.lua
@@ -36,6 +36,8 @@ function love.load()
         resizeble = true
     })
 
+    love.keyboard.keysPressed = {}
+
 end
 
 function love.resize(w,h)
@@ -43,10 +45,23 @@ function love.resize(w,h)
 end
 
 function love.keypressed(key)
+
+    love.keyboard.keysPressed[key] = true
+
     if key == 'escape' then
         love.event.quit()
     end
+
 end
+
+function love.keyboard.wasPressed(key)
+    if love.keyboard.keysPressed[key] then
+        return true
+    else
+        return false
+    end
+end
+
 
 function love.update(dt)
 
@@ -58,7 +73,12 @@ function love.update(dt)
 
     bird:update(dt)
 
+    love.keyboard.keysPressed = {}
+
 end
+
+
+
 
 
 function love.draw()
